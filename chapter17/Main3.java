49f4bf6481abcf08469764e9e8e4c4d4b1611f2b
naorex <nao23r@gmail.com>
2025-12-09 13:05:04 +0000

    UPDAE : today's progress (page 661)

--- chapter17/Main3.java
+++ chapter17/Main3.java
@@ -1,18 +1,17 @@
 import java.awt.FlowLayout;
 import javax.swing.*;
 
-public class Main2 {
+public class Main3 {
   public static void main(String[] args) {
     JFrame frame = new JFrame("はじめてのSwing");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 200);
-
-    // Widget の定義と追加
-    // 内部で ContentPane というものを作り、それに add していく構造
     frame.setLayout(new FlowLayout());
-    JLabel label = new JLabel("Hello World!!");
+
+    JLabel label = new JLabel("Hello World!");
     frame.add(label);
     JButton button = new JButton("押してね");
+    button.addActionListener(new MinatoListener()); // ボタンが押されたら起動
     frame.add(button);
 
     frame.setVisible(true);
